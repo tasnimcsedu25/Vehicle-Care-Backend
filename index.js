c849378ea4d781b920dbbf4a43147c4ccce89385
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2024-08-07 00:01:22 +0600

    vehicle apiendpoints

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -29,62 +28,94 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Vehicle Care API');
 });
 
-// Get all users
+// User routes
 app.get('/users', (req, res) => {
-    const sql = 'SELECT * FROM user';
+    const sql = 'SELECT * FROM users';
     db.query(sql, (err, results) => {
         if (err) throw err;
         res.json(results);
     });
 });
 
-// Get a single user by UserID
 app.get('/users/:UserID', (req, res) => {
-    const sql = 'SELECT * FROM user WHERE UserID = ?';
+    const sql = 'SELECT * FROM users WHERE UserID = ?';
     db.query(sql, [req.params.UserID], (err, result) => {
         if (err) throw err;
         res.json(result);
     });
 });
-
-// Add a new user
 
 app.post('/users', (req, res) => {
-    console.log(req.body);
     const newUser = req.body;
-    const sql = 'INSERT INTO user (UserID, `User Name`, Email, Password) VALUES (?, ?, ?, ?)';
-    db.query(sql, [newUser.UserID, newUser['UserName'], newUser.Email, newUser.Password], (err, result) => {
+    const sql = 'INSERT INTO users (UserName, Email, Password) VALUES (?, ?, ?)';
+    db.query(sql, [newUser.UserName, newUser.Email, newUser.Password], (err, result) => {
         if (err) throw err;
         res.json(result);
     });
-    
 });
 
-
-// Update a user by UserID
 app.put('/users/:UserID', (req, res) => {
     const updatedUser = req.body;
-    const sql = 'UPDATE user SET `User Name` = ?, Email = ?, Password = ? WHERE UserID = ?';
-    db.query(sql, [updatedUser['UserName'], updatedUser.Email, updatedUser.Password, req.params.UserID], (err, result) => {
+    const sql = 'UPDATE users SET UserName = ?, Email = ?, Password = ? WHERE UserID = ?';
+    db.query(sql, [updatedUser.UserName, updatedUser.Email, updatedUser.Password, req.params.UserID], (err, result) => {
         if (err) throw err;
         res.json(result);
     });
 });
 
-// Delete a user by UserID
 app.delete('/users/:UserID', (req, res) => {
-    const sql = 'DELETE FROM user WHERE UserID = ?';
+    const sql = 'DELETE FROM users WHERE UserID = ?';
     db.query(sql, [req.params.UserID], (err, result) => {
         if (err) throw err;
         res.json(result);
     });
 });
 
-app.listen(port,"0.0.0.0", () => {
+// Vehicle routes
+app.get('/vehicles', (req, res) => {
+    const sql = 'SELECT * FROM vehicles';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.get('/vehicles/:vehicleID', (req, res) => {
+    const sql = 'SELECT * FROM vehicles WHERE vehicleID = ?';
+    db.query(sql, [req.params.vehicleID], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+app.post('/vehicles', (req, res) => {
+    const newVehicle = req.body;
+    const sql = 'INSERT INTO vehicles (model, engineNumber, chesisNumber) VALUES (?, ?, ?)';
+    db.query(sql, [newVehicle.model, newVehicle.engineNumber, newVehicle.chesisNumber], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+app.put('/vehicles/:vehicleID', (req, res) => {
+    const updatedVehicle = req.body;
+    const sql = 'UPDATE vehicles SET model = ?, engineNumber = ?, chesisNumber = ? WHERE vehicleID = ?';
+    db.query(sql, [updatedVehicle.model, updatedVehicle.engineNumber, updatedVehicle.chesisNumber, req.params.vehicleID], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+app.delete('/vehicles/:vehicleID', (req, res) => {
+    const sql = 'DELETE FROM vehicles WHERE vehicleID = ?';
+    db.query(sql, [req.params.vehicleID], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
 
 module.exports = db;
-
-
-
